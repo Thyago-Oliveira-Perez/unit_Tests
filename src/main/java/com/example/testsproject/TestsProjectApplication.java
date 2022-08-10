ef91aec6d783ca64ce7970d9f5ddeb79a3e98427
Thyago_Oliveira_Perez <thyagodoliveiraperez@gmail.com>
2022-08-09 21:31:52 -0300

    first commit

---
+++ src/main/java/com/example/testsproject/TestsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsProjectApplication.class, args);
+    }
+
+}
